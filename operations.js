2e4f014ea137b6d9f538d050dea71dbe376f230b
JAKThaDev <jdakemp@gmail.com>
2021-04-19 21:02:17 -0400

    replace err handling with promises started

---
+++ operations.js
@@ -0,0 +1,23 @@
+const assert = require('assert').strict;  
+
+
+exports.insertDocument = (db, document, collection) => {
+    const coll = db.collection(collection); 
+    coll.insertOne(document) 
+};
+
+exports.findDocuments = (db, collection) => {
+    const coll = db.collection(collection); 
+    return coll.find().toArray()
+};
+
+exports.removeDocument = (db, document, collection) => {
+    const coll = db.collection(collection); 
+    coll.deleteOne(document);
+};
+
+exports.updateDocument = (db, document, update, collection) => {
+    const coll = db.collection(collection); 
+    return coll.updateOne(document, {$set: update }, null);
+
+};
